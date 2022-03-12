99a351c2446a20fc1d672d6e2ce970b83a0e7682
ShyamPraveenSingh <shyampaveensingh@gmail.com>
2022-03-12 18:17:42 +0530

    new files and changes

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './App.css';
+import './style.css'
+
+const App = () => {
+  const [quotes, setQuotes] = useState('');
+  const textRef = useRef();
+  let colors = ["#ffff00", "#90ee90", "#ffa500", "ff68ff", "#a9a9e7",];
+
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+    .then((res) => res.json())
+    .then((data) => {
+      let randomNum = Math.floor(Math.random() * data.length);
+      setQuotes(data[randomNum]);
+    });
+  };
+
+  useEffect(() =>{
+    getQuote();
+  }, []);
+
+  useEffect(() =>{
+    textRef.current.style.color = colors[Math.floor(Math.random() * colors.length)]
+  }, [quotes]);
+
+  return(
+    <div className="App">
+      <div className='quote'>
+      <p ref={textRef}>{quotes.text}</p>
+      <p>{quotes.author}</p>
+        <div className='btnContainer'>
+          <button onClick={getQuote} className='btn'>
+            Get Quote</button>
+          <a 
+          href={`https://twitter.com/intent/tweet?text=${quotes.text}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className='btn'>
+            Tweet
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
